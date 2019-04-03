c89ae70aed91c28e87b66054b6b68e4ce46cd109
Molly <mollyerskine96@gmail.com>
2019-04-03 19:01:51 +0100

    Update data-check.js
    Added comments

--- js/data-check.js
+++ js/data-check.js
@@ -8,7 +8,7 @@ function isDrawable(b) {
     }
 
     if (b.bb_drop == null) {
-        //bb_drop cannot be calculated\ from other parameters
+        //bb_drop cannot be calculated from other parameters
         logBikeMessage("No bb_drop");
         return false;
     }
@@ -20,16 +20,19 @@ function isDrawable(b) {
             return false;
         } else {
             //wheelbase can be calculated from other parameters
+            //Pythagorus thereom is used to calculate the two right angled triangles Lbb_drop/chainstay/wheelbaseLEFT and Lbb_drop/front_centre/wheelbaseRIGHT
             b.wheelbase = Math.sqrt(Math.pow(b.chainstay, 2) - Math.pow(b.bb_drop, 2)) + Math.sqrt(Math.pow(b.front_centre, 2) - Math.pow(b.bb_drop, 2));
         }
     }
 
     if (b.head_angle == null && b.stack == null) {
+        //head_angle cannot be calculated from other parameters
         logBikeMessage("No stack or head_angle");
         return false;
     }
 
     if (b.seat_angle == null) {
+        //seat_angle cannot be calculated from other parameters
         logBikeMessage("No seat_angle");
         return false;
     }
@@ -40,39 +43,51 @@ function isDrawable(b) {
             logBikeMessage("No stack");
             return false;
         } else if (b.seat_tube_length_eff != null) {
+            //stack can be calculated from other parameters
+            //Sine Rule -> O = seat_tube_lenghth and Theta = seat_angle
             b.stack = b.seat_tube_length_eff * Math.sin(b.seat_angle);
         } else if (b.standover == null || b.bb_height == null) {
             //stack can be calculated from other parameters
+            //From bb_drop bottom to top of stack = bb_drop + Sine Rule -> O = head_tube and Theta = head_angle + Sine Rule - O = fork_length and Theta = head_angle
             b.stack = b.bb_drop + (Math.sin(b.head_angle * Math.PI / 180) * b.head_tube) + (b.fork_length * Math.sin(b.head_angle * Math.PI / 180));
         } else {
             //stack can be calculated from other parameters
+            //Full height of bike (standover) - height wheelbase to ground (bb_height) + bb_drop
             b.stack = b.standover - b.bb_height + b.bb_drop;
         }
     }
 
     if (b.head_angle == null) {
         if (b.head_tube == null || b.fork_length == null) {
+            //head_angle cannot be calculated from other parameters
             logBikeMessage("No head_angle");
             return false
         }
+        //head_angle can be calculated from other parameters
+        //(Inverse) Sine Rule - H = stack and O = head_tube + forklength
         b.head_angle = (b.stack) / Math.asin(b.head_tube + b.fork_length);
     }
 
     if (b.head_tube == null) {
         if (b.fork_length == null) {
+            //if both headtube and fork length are null we cannot draw
             logBikeMessage("No head_tube or fork_length");
             return false;
         } else {
+            //if only head tube is null then we can calculate it using head angle (already found) and fork length
+            //Sine Rule - O = stack and Theta = head_angle and - fork_length
             b.head_tube = (b.stack / Math.sin(b.head_angle)) - b.fork_length;
         }
     }
 
     if (b.fork_rake == null) {
+        //if no fork rake angle is provided we assume that the is no change in the angle
         logBikeMessage("No fork_rake, assumption is that there is no change in angle");
         b.fork_rake = 0;
     }
 
     if (b.seat_tube_length == null) {
+        //See if any kind of seat tube is present and use that value
         if (b.seat_tube_length_cc != null) {
             b.seat_tube_length = b.seat_tube_length_cc
         } else if (b.seat_tube_length_eff != null) {
@@ -86,6 +101,7 @@ function isDrawable(b) {
     return true;
 }
 
+//Used to log "errors" to console
 function logBikeMessage(message) {
     console.info(("Bike check: " + message));
 }
